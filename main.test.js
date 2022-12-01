e84407620375180686f9f22853a412bffb9c244b
Gio <69569383+gioguevara@users.noreply.github.com>
2022-12-01 13:08:16 +0800

    re-tested search function

--- main.test.js
+++ main.test.js
@@ -2,21 +2,26 @@ import { sort, search, newsList } from "./main.js";
 
 //Example testing for search function
 // test("Search function testing", () => {
-//   expect(search(newsList)).toBe("I am a cool web developer");
+//    expect(search(newsList)).toBe("I am a cool web developer");
 // });
 
-test('has an undefined input', () => {
+test("search function test", () => {
+    expect(JSON.stringify(search)).toBe();
+  })
+
+
+test('if search has an undefined input', () => {
   expect(search.newsList).toBeUndefined();
 })
 
-test('to contain an equal value', () => {
+test('if newslist contains an equal value', () => {
   expect(newsList).toContainEqual("What films should an aspiring journalist watch?");
 })
 
-test('to sort in ascending order', () => {
+test('if search can sort in ascending order', () => {
   expect(sort(`ascending`)).toEqual(newsList.sort())
 })
 
-test('to sort in descending order', () => {
+test('if search can sort in descending order', () => {
   expect(sort(`descending`)).toEqual(newsList.reverse())
 })
